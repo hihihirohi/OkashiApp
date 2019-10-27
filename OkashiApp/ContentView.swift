5cec597191d275cf06911f7c1cf27876dfdab3c4
hihihirohi <hirothugu326@gmail.com>
2019-10-27 17:04:11 +0900

    first commit

---
+++ OkashiApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OkashiApp
+//
+//  Created by 白岩ひろつぐ on 2019/10/27.
+//  Copyright © 2019 shiraiwa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# OkashiApp
